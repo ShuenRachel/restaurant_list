3a8e9e6d1052ff1d8360893180902e2e30713d73
ShuenRachel <shuen0410@gmail.com>
2021-10-21 16:31:35 +0800

    Initialized project

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index', { restaurants: restaurantList.results })
+})
+
+app.get('/search', (req, res) => {
+  const keywords = req.query.keyword.toLowerCase().trim()
+  const restaurants = restaurantList.results.filter(restaurant => {
+    return restaurant.name.toLowerCase().includes(keywords) || restaurant.name_en.toLowerCase().includes(keywords) ||
+    restaurant.category.toLowerCase().includes(keywords)
+  })
+  if(!restaurants.length) {
+    console.log('No search result')
+  }
+
+  res.render('index', { restaurants: restaurants, keywords:  keywords })
+})
+
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+  res.render('show', { restaurant: restaurant })
+})
+
+app.listen(port, () => {
+  console.log(`Restaurant List is running on http://localhost:${port}`)
+})
