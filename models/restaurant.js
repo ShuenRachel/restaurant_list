f1bec4f7895ed172f96b76986429589daaca3914
ShuenRachel <shuen0410@gmail.com>
2021-10-29 23:57:08 +0800

    feat: add restaurant seeder

--- models/restaurant.js
+++ models/restaurant.js
@@ -1,35 +1,16 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+
 const restaurantSchema = new Schema({
-  name: {
-    type: String
-    required: true
-  },
-
-  name_en: String,
-
-  category: {
-    type: String
-    required: true
-  },
-
-  image: String,
-
-  location: {
-    type: String
-    required: true
-  },
-
-  phone: String,
-
-  google_map: String,
-
-  rating: Number,
-
-  description: {
-    type: String
-    required: true
-  },
+  name: { type: String, required: true },
+  name_en: { type: String, required: true },
+  category: { type: String, required: true },
+  image: { type: String, required: true },
+  location: { type: String, required: true },
+  phone: { type: String, required: true },
+  google_map: { type: String, required: true },
+  rating: { type: Number, required: true },
+  description: { type: String, required: true },
 })
 
 module.exports = mongoose.model('Restaurant', restaurantSchema)
